ff1ddf46905bcdb25c17bb4ea56291dd6dc23164
Christopher Moore <christopher.e.moore@gmail.com>
2016-11-28 21:03:41 +0200

    added test

---
+++ test.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/env luajit
+local vec3 = require 'vec.vec3'
+local jfnk = require 'jfnk'
+
+-- a cross b = c, solve for b
+print(jfnk{
+	x = vec3(-1,-1,-1),
+	f = function(x)
+		return vec3.cross({1,0,0}, x) - {0,0,1}
+	end,
+})
+
+-- current is the divergence of the EM vector potential
+-- so how about an inverse divergence function? 
+-- in flat space that is just as easy solving for each xyz individually
+-- in curved space the components mix together a bit 
